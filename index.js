354c250411fc928ddeb53186c440d93117533010
Charlie Friend <charles.d.friend@gmail.com>
2018-04-19 22:51:45 -0700

    Add github webhook handler

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 "use strict";
 
 const express = require('express');
-const github = require('github-webhook-handler');
+const githubHandler = require('github-webhook-handler');
 
 var app = express();
 
-app.get("/", (req, res) => {
+// Middleware for handling Github Webhooks
+var handler = githubHandler({
+    path: '/',
+    secret: process.env.WEBHOOK_SECRET
+});
 
-    res.send("Hello world!");
+handler.on('issue_comment', event => {
+
+    console.log("Got issue_comment");
+    console.log(event)
 
 });
 
-app.listen(process.env.PORT || 50000, function() {
+app.use(handler);
+
+app.listen(process.env.PORT || 50000, () => {
 
     console.log("Server listening on port " + (process.env.PORT || 50000));
 
-});
+})
