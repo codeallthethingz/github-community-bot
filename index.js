f9f60518fe9b82e0c0969bbd36b1caa304a993b0
Charlie Friend <charles.d.friend@gmail.com>
2018-04-19 23:39:31 -0700

    Handle '/assign to me' command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const express = require('express');
+const express       = require('express');
+const githubApp     = require('github-app');
 const githubHandler = require('github-webhook-handler');
 
 var app = express();
@@ -14,7 +15,24 @@ var handler = githubHandler({
 handler.on('issue_comment', event => {
 
     console.log("Got issue_comment");
-    console.log(event)
+    console.log(event);
+
+    // Only respond if comment was created or edited
+    var action = event.payload.action;
+    if (!(action == 'created' || action == 'edited')) {
+        return;
+    }
+
+    // User requests issue assignment using the '/assign to me' command
+    if (event.payload.comment.body == '/assign to me') {
+
+        // TODO: issue must be triaged before assigning
+
+        // TODO: user must be approved to work in the project
+
+        console.log("Assigning to user");
+
+    }
 
 });
 
@@ -24,4 +42,4 @@ app.listen(process.env.PORT || 50000, () => {
 
     console.log("Server listening on port " + (process.env.PORT || 50000));
 
-})
+});
