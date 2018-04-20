add4dad29cd63a88fe4ccb5c769f0a71500509c3
Charlie Friend <charles.d.friend@gmail.com>
2018-04-19 21:49:52 -0700

    Add files for Heroku

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const express = require('express');
 const github = require('github-webhook-handler');
 
 var app = express();
+
+app.get("/", (req, res) => {
+
+    res.send("Hello world!");
+
+});
+
+app.listen(process.env.port || 50000, function() {
+
+    console.log("Server listening on port " + (process.env.port || 50000));
+
+});
